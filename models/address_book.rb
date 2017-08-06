4e4ac109b1ed564b9a62d0289cf9749bc0381580
Rami Hamdan <hamdance0@gmail.com>
2017-08-06 21:13:10 +0200

    importing data from a csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -18,4 +19,23 @@ class AddressBook
 
     entries.insert(index,Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    entries.each_with_index do |entry, index|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entries.delete_at(index)
+      end
+    end
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
 end
